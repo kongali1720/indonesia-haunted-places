23233293764169bc77f54a86902f8ba5e37792a1
kongali1720 <121486486+kongali1720@users.noreply.github.com>
2025-06-08 02:35:34 +0800

    Create 81_100_indonesia_haunted_places_data.sql

---
+++ 81_100_indonesia_haunted_places_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO haunted_places (name, location, description, history, reported_sightings) VALUES
+('Rumah Hantu Taman Kencana', 'Bogor', 'Rumah tua dengan aktivitas gaib.', 'Dulunya rumah keluarga kaya.', 'Warga melaporkan penampakan dan suara.'),
+('Gedung BNI, Jakarta', 'Jakarta', 'Gedung perkantoran dengan cerita hantu.', 'Dulunya kantor lama.', 'Pegawai melaporkan suara aneh.'),
+('Rumah Miring, Yogyakarta', 'Yogyakarta', 'Rumah yang dikenal dengan penampakan.', 'Dulunya milik keluarga besar.', 'Orang sering melihat bayangan.'),
+('Taman Suropati, Jakarta', 'Jakarta', 'Taman kota dengan cerita mistis.', 'Sering dijadikan tempat ritual.', 'Pengunjung melaporkan suara gaib.'),
+('Jalan Braga, Bandung', 'Bandung', 'Jalan bersejarah dengan cerita angker.', 'Banyak peristiwa masa lalu.', 'Orang melaporkan penampakan.'),
+('Gedung Sate, Bandung', 'Bandung', 'Gedung pemerintahan dengan cerita hantu.', 'Dulunya kantor kolonial.', 'Pegawai melaporkan penampakan.'),
+('Rumah Pengabdi Setan, Jakarta', 'Jakarta', 'Rumah dengan cerita mistis.', 'Dulunya tempat kejadian aneh.', 'Warga sering melihat sosok.'),
+('Pantai Nglambor', 'Yogyakarta', 'Pantai dengan legenda mistis.', 'Dipercaya dihuni makhluk halus.', 'Wisatawan melaporkan penampakan.'),
+('Museum Bank Indonesia', 'Jakarta', 'Museum dengan cerita supranatural.', 'Bangunan tua.', 'Pengunjung melaporkan suara dan bayangan.'),
+('Hutan Pinus Mangunan', 'Yogyakarta', 'Hutan pinus dengan cerita mistis.', 'Tempat berkumpul makhluk halus.', 'Pengunjung sering mendengar suara.');
