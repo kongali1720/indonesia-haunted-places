a0b745ebb5ab0204736eda0b244ea16a254fc592
kongali1720 <121486486+kongali1720@users.noreply.github.com>
2025-06-08 02:33:36 +0800

    Create 21_40_indonesia_haunted_places_data.sql

---
+++ 21_40_indonesia_haunted_places_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO haunted_places (name, location, description, history, reported_sightings) VALUES
+('Pulau Kelor, Kepulauan Seribu', 'Jakarta', 'Pulau kecil dengan cerita misterius.', 'Dulu tempat pembuangan dan kejahatan.', 'Pengunjung sering mendengar suara aneh dan penampakan.'),
+('Situs Gunung Padang', 'Cianjur, Jawa Barat', 'Situs megalitikum yang menyimpan misteri dan cerita gaib.', 'Dikenal sebagai situs tertua.', 'Peneliti melaporkan kejadian aneh.'),
+('Lawang Sewu Lantai Bawah', 'Semarang', 'Bagian bawah gedung yang sangat angker.', 'Dulunya penjara bawah tanah.', 'Sering terlihat bayangan dan suara jeritan.'),
+('Puncak Gunung Merapi', 'Yogyakarta', 'Gunung berapi dengan cerita mistis.', 'Sering terjadi letusan dan aktivitas gaib.', 'Pendaki melaporkan penampakan dan suara aneh.'),
+('Rumah Hantu Jatibening', 'Bekasi, Jawa Barat', 'Rumah kosong yang terkenal dengan aktivitas gaib.', 'Dulunya tempat pembantaian.', 'Warga sekitar sering melihat sosok dan suara.'),
+('Hutan Gunung Semeru', 'Jawa Timur', 'Gunung tertinggi di Jawa dengan cerita angker.', 'Konon dihuni makhluk halus penjaga.', 'Pendaki sering mendengar suara aneh dan melihat cahaya.'),
+('Pantai Klayar', 'Pacitan, Jawa Timur', 'Pantai dengan legenda dan cerita mistis.', 'Dikenal dengan batu karang yang unik.', 'Wisatawan melaporkan suara gaib dan penampakan.'),
+('Rumah Sakit Dr. Cipto Mangunkusumo', 'Jakarta', 'RS besar dengan banyak cerita hantu.', 'Banyak tragedi medis.', 'Staf melaporkan penampakan dan suara aneh.'),
+('Monumen Nasional (Monas)', 'Jakarta', 'Monumen ikonik dengan cerita mistis.', 'Dibangun di atas tanah yang dianggap keramat.', 'Orang melaporkan suara gaib dan bayangan.'),
+('Gedung Filateli, Jakarta', 'Jakarta', 'Gedung tua dengan aktivitas supranatural.', 'Dulunya kantor pos.', 'Karyawan melaporkan suara aneh dan penampakan.');
