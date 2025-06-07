4a5eb2e9a19494ae2a53e320dfd5f02b3c071f0b
kongali1720 <121486486+kongali1720@users.noreply.github.com>
2025-06-08 02:35:04 +0800

    Create 61_80_indonesia_haunted_places_data.sql

---
+++ 61_80_indonesia_haunted_places_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO haunted_places (name, location, description, history, reported_sightings) VALUES
+('Pantai Selatan, Jawa Selatan', 'Jawa Selatan', 'Pantai dengan legenda Ratu Kidul.', 'Dipercaya dihuni makhluk halus.', 'Pengunjung sering melihat sosok misterius.'),
+('Hutan Mangrove Angke', 'Jakarta Utara', 'Hutan dengan cerita mistis.', 'Dulunya tempat bersembunyi.', 'Pengunjung melaporkan suara dan penampakan.'),
+('Rumah Sakit PKU Muhammadiyah', 'Yogyakarta', 'RS dengan cerita hantu.', 'Dulunya rumah sakit lama.', 'Staf melaporkan suara dan penampakan.'),
+('Museum Wayang', 'Jakarta', 'Museum dengan cerita mistis.', 'Menampung wayang tua.', 'Pengunjung melaporkan suara gaib.'),
+('Kawasan Kota Tua Makassar', 'Makassar', 'Area bersejarah dengan cerita angker.', 'Pusat aktivitas masa lalu.', 'Orang melaporkan penampakan dan suara.'),
+('Taman Nasional Gunung Leuser', 'Sumatera Utara', 'Gunung dengan cerita makhluk halus.', 'Hutan lebat dengan misteri.', 'Pendaki melaporkan suara aneh.'),
+('Pusat Perbelanjaan Mangga Dua', 'Jakarta', 'Mall dengan cerita mistis.', 'Banyak kejadian aneh.', 'Pengunjung melaporkan penampakan.'),
+('Kawah Putih, Bandung', 'Bandung, Jawa Barat', 'Danau kawah dengan legenda.', 'Dulunya tempat ritual.', 'Pengunjung melihat penampakan.'),
+('Pasar Santa, Jakarta', 'Jakarta Selatan', 'Pasar dengan cerita mistis.', 'Banyak kejadian aneh.', 'Pengunjung melaporkan suara dan bayangan.'),
+('Gua Maria Kerep Ambarawa', 'Semarang', 'Gua dengan cerita mistis.', 'Tempat ziarah.', 'Pengunjung melaporkan penampakan.');
