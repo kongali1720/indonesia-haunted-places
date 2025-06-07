8b5818c1c091650dff11e4dd0257d68f6080ce78
kongali1720 <121486486+kongali1720@users.noreply.github.com>
2025-06-08 02:34:25 +0800

    Create 41_60_indonesia_haunted_places_data.sql

---
+++ 41_60_indonesia_haunted_places_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO haunted_places (name, location, description, history, reported_sightings) VALUES
+('Kawasan Kota Lama Semarang', 'Semarang', 'Area bersejarah dengan banyak cerita hantu.', 'Dulunya pusat perdagangan.', 'Orang sering melihat bayangan dan mendengar suara.'),
+('Pulau Onrust', 'Kepulauan Seribu', 'Pulau dengan sejarah kelam.', 'Dulunya tempat pembuangan.', 'Pengunjung melaporkan penampakan dan suara aneh.'),
+('Benteng Rotterdam', 'Makassar, Sulawesi Selatan', 'Benteng tua dengan cerita mistis.', 'Dulunya pusat pertahanan.', 'Pengunjung melaporkan suara gaib dan penampakan.'),
+('Rumah Pohon, Bali', 'Bali', 'Tempat dengan cerita mistis dan penampakan.', 'Dulunya tempat ritual.', 'Orang melaporkan suara dan penampakan aneh.'),
+('Gunung Bromo', 'Jawa Timur', 'Gunung dengan legenda dan cerita gaib.', 'Tempat upacara adat.', 'Pendaki sering melihat penampakan.'),
+('Taman Sari Water Castle', 'Yogyakarta', 'Bekas taman kerajaan dengan cerita mistis.', 'Pernah menjadi tempat rekreasi Sultan.', 'Pengunjung sering melihat bayangan dan mendengar suara.'),
+('Rumah Sakit Dr. Sardjito', 'Yogyakarta', 'RS dengan cerita hantu.', 'Banyak kejadian tragis.', 'Staf sering melaporkan penampakan.'),
+('Jembatan Pasupati', 'Bandung, Jawa Barat', 'Jembatan dengan legenda dan cerita mistis.', 'Banyak kecelakaan.', 'Pengendara melaporkan penampakan.'),
+('Taman Wisata Alam Mangrove', 'Jakarta', 'Tempat dengan cerita mistis.', 'Dulunya area penuh misteri.', 'Pengunjung sering mendengar suara dan melihat bayangan.'),
+('Rumah Sakit Fatmawati', 'Jakarta', 'Rumah sakit dengan aktivitas supranatural.', 'Banyak cerita hantu.', 'Staf dan pasien sering melaporkan kejadian aneh.');
