b0316de85dcb6208f73937367a9dcd0ce225355e
kongali1720 <121486486+kongali1720@users.noreply.github.com>
2025-06-08 02:32:39 +0800

    Create 11_20_indonesia_haunted_places_data.sql

---
+++ 11_20_indonesia_haunted_places_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO haunted_places (name, location, description, history, reported_sightings) VALUES
+('Pantai Parangtritis', 'Yogyakarta', 'Pantai terkenal dengan cerita Ratu Kidul dan penampakan makhluk halus.', 'Legenda Ratu Kidul yang menguasai laut selatan.', 'Banyak wisatawan melaporkan penampakan sosok wanita berambut panjang.'),
+('Pulau Kembang, Lampung', 'Lampung', 'Pulau yang dikenal angker dan penuh misteri.', 'Dulunya tempat pengasingan dan kejadian aneh.', 'Sering terdengar suara misterius dan penampakan.'),
+('Hotel Lawang, Surabaya', 'Surabaya', 'Hotel tua dengan sejarah kelam dan aktivitas gaib.', 'Pernah terjadi pembunuhan di hotel ini.', 'Pengunjung sering mendengar suara aneh dan melihat bayangan.'),
+('Hutan Gunung Salak', 'Bogor, Jawa Barat', 'Hutan angker dengan banyak cerita penampakan dan kejadian mistis.', 'Tempat ini dikenal sebagai sarang makhluk halus.', 'Pendaki sering melihat cahaya dan suara aneh.'),
+('Jembatan Ancol', 'Jakarta Utara', 'Jembatan yang terkenal dengan cerita mistis dan penampakan.', 'Banyak kecelakaan misterius yang terjadi.', 'Pengendara melaporkan melihat sosok hantu di malam hari.'),
+('Rumah Sakit Dr. Soetomo', 'Surabaya', 'Rumah sakit dengan cerita hantu pasien yang gentayangan.', 'Banyak cerita tragedi dan kematian di sini.', 'Staf dan pengunjung sering melihat penampakan.'),
+('Jalan Malioboro, Yogyakarta', 'Yogyakarta', 'Jalan legendaris yang juga punya cerita mistis.', 'Banyak peristiwa sejarah dan kejadian aneh.', 'Orang sering mendengar suara aneh dan melihat bayangan.'),
+('Museum Benteng Vredeburg', 'Yogyakarta', 'Museum bekas benteng dengan cerita hantu tentara.', 'Dulunya benteng pertahanan.', 'Pengunjung melaporkan penampakan tentara dan suara.'),
+('Taman Mini Indonesia Indah (TMII)', 'Jakarta Timur', 'Area taman budaya yang konon memiliki penampakan.', 'Beberapa cerita mistis terkait pembangunan.', 'Pengunjung melaporkan penampakan dan suara aneh.'),
+('Rumah Tua Taman Sari', 'Yogyakarta', 'Bangunan tua dengan cerita mistis.', 'Pernah menjadi pusat kerajaan.', 'Sering terlihat penampakan wanita berpakaian kuno.');
